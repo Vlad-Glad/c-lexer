2c6718c90292afdfefc42e9865ed8f69910037f5
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2025-10-01 18:15:27 +0300

    Token.hpp first commit

---
+++ include/lexer/Token.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+
+namespace clex {
+
+	enum class TokenKind {
+		Identifier,
+		Keyword,
+		IntLiteral,
+		FloatLiteral,
+		CharLiteral,
+		StringLiteral,
+		Preprocessor,
+		Comment,
+		Operator,
+		Punctuator,
+		Ellipsis,
+		MacroHash, 
+		MacroConcat,
+		Error, 
+		EndOfFile
+	};
+
+	struct SourcePos {
+		int line = 1;
+		int column = 1;
+	};
+
+
+	struct Token {
+		TokenKind kind{};
+		std::string lexeme;
+		SourcePos pos{};
+		std::string message;
+	};
+
+	std::string to_string(TokenKind kind);
+
+}
