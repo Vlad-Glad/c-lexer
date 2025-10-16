96c22453bc5e37fceb0c799c240bf16aadd774ca
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2025-10-16 19:18:13 +0300

    implement to_string in token.cpp

---
+++ src/Token.cpp
@@ -0,0 +1,24 @@
+#include "lexer/Token.hpp"
+
+namespace clex {
+    std::string to_string(TokenKind k) {
+        switch (k) {
+        case TokenKind::Identifier:   return "Identifier";
+        case TokenKind::Keyword:      return "Keyword";
+        case TokenKind::IntLiteral:   return "IntLiteral";
+        case TokenKind::FloatLiteral: return "FloatLiteral";
+        case TokenKind::CharLiteral:  return "CharLiteral";
+        case TokenKind::StringLiteral:return "StringLiteral";
+        case TokenKind::Preprocessor: return "Preprocessor";
+        case TokenKind::Comment:      return "Comment";
+        case TokenKind::Operator:     return "Operator";
+        case TokenKind::Punctuator:   return "Punctuator";
+        case TokenKind::Ellipsis:     return "Ellipsis";
+        case TokenKind::MacroHash:    return "MacroHash";
+        case TokenKind::MacroConcat:  return "MacroConcat";
+        case TokenKind::Error:        return "Error";
+        case TokenKind::EndOfFile:    return "EOF";
+        }
+        return "Unknown";
+    }
+}
